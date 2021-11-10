912214aac4b2d8e9f913b0369aa3d715a1b1d745
Lihi <lihise9@gmail.com>
2021-11-10 13:27:48 +0200

    changing to notes

---
+++ js/apps/keep/pages/note-app.cmp.js
@@ -0,0 +1,10 @@
+//NOTES PAGE
+
+export default {
+    name: 'note',
+    template: `
+        <section class="note-app main-app">
+            <h1>Note</h1>
+        </section>
+    `
+};
--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import homePage from './pages/home-page.cmp.js'
 import aboutPage from './pages/about-page.cmp.js'
 import mailApp from './apps/mail/pages/mail-app.cmp.js'
-import keepApp from './apps/keep/pages/keep-app.cmp.js'
+import noteApp from './apps/keep/pages/note-app.cmp.js'
 // import carDetails from './pages/car-details.cmp.js'
 // import carSurvey from './pages/car-survey.cmp.js'
 // import carEdit from './pages/car-edit.cmp.js'
@@ -31,7 +31,7 @@ const routes = [
   },
   {
     path: '/keep',
-    component: keepApp,
+    component: noteApp,
   },
   // {
   //   path: '/car/:carId?/edit',
