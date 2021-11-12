254169e4eff165a11a5fc0cbf4a8d8fc50a56476
Ortal <ortalcn@mta.ac.il>
2021-11-12 21:51:50 +0200

    style select element

--- js/apps/mail/cmps/mail-main-area-nav.cmp.js
+++ js/apps/mail/cmps/mail-main-area-nav.cmp.js
@@ -2,12 +2,9 @@ export default {
     name: 'mail-main-area-nav',
     template: `
         <nav class="mail-main-area-nav flex space-between">
-            <!-- dropdown css adjustments -->
             <section class="flex space-between">
-                <button class="btn-mail-icon" @click="toggleFilterShow" title="Filter by" ><i class="fas fa-filter"></i></button>
-                <select v-if="this.isFilterClicked" v-model="filtered" @change="filter" style="width:100px;">
-                    <option focus></option>
-                    <option value="all">All</option>
+                <select v-model="filtered" @change="filter" style="width:100px;">
+                    <option value="all">Filter By</option>
                     <option value="read">Read</option>
                     <option value="starred">Starred</option>
                     <option value="unread">Unread</option>
@@ -15,19 +12,17 @@ export default {
                 </select>
             </section>
             <input v-model="filtered" @input="filter" class="mail-input" type="text" placeholder="Search..."/>
-            <!-- dropdown css adjustments -->
             <select v-model="sorted" title="Sort by" @change="sort" style="width:100px;">
+                <option value="dateNTO">Sort By</option>
                 <option value="dateNTO">Date (New to old)</option>
                 <option value="dateOTN">Date (Old to new)</option>
                 <option value="titleAO">Title (Ascending order)</option>
                 <option value="titleDO">Title (Descending order)</option>
             </select>
-            
         </nav>
     `,
     data() {
         return {
-            isFilterClicked: false,
             filtered: 'all',
             sorted: 'dateNTO',
         }
@@ -35,9 +30,6 @@ export default {
     methods: {
         filter() {
             this.$emit('filter', `${this.filtered}`)
-        },
-        toggleFilterShow() {
-            this.isFilterClicked = !this.isFilterClicked
         },
         sort() {
             this.$emit('sort', `${this.sorted}`)
